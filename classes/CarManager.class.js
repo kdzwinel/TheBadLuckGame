108c07db31cef458def2a7cd213610263b5a76a9
Paweł Bród <mwadmin@mwadmins-MacBook-Pro.local>
2013-09-02 14:49:30 +0200

    add eventListener to Car.class

--- classes/CarManager.class.js
+++ classes/CarManager.class.js
@@ -11,6 +11,28 @@
 			_cars       = [],
 			_toRadians  = Math.PI/180;
 
+		function _removeCar(carReference) {
+			var i = _cars.length
+
+			while(i--) {
+				if(carReference === _cars[i]) {
+					_cars.splice(i,1);	
+					game.carLost();
+				}
+			}
+		}
+
+		function _carTripEnd(carReference) {
+			var i = _cars.length
+
+			while(i--) {
+				if(carReference === _cars[i]) {
+					_cars.splice(i,1);	
+					game.carWon();
+				}
+			}
+		}
+		
 		//PUBLIC
 
 		this.addCar = function(options) {
@@ -23,39 +45,42 @@
                 startX    = options.startX || startTile.getX(),
                 startY    = options.startY || startTile.getY(),
                 endX      = options.endX || endTile.getX(),
-                endY      = options.endY || endTile.getY();
+                endY      = options.endY || endTile.getY(),
+                
+                car = new Car({
+                	startTileX : startX,
+	                startTileY : startY,
+	                endTileX   : endX,
+	                endTileY   : endY,
+	                type       : carType,
+	                board      : _board,
+	                tileSize   : 200
+	            });
 
-            _cars.push(new Car({startTileX : startX,
-                startTileY : startY,
-                endTileX   : endX,
-                endTileY   : endY,
-                type       : carType,
-                board      : _board,
-                tileSize   : 200}));
+	        car.on('crash', _removeCar);
+	        car.on('trip-end', _carTripEnd);       
+
+            _cars.push(car);
 
 		};
 
 		this.render = function(context) {
 			var i = _cars.length;
 
-			while(i) {
-				if(_cars[--i].alive) {
-					context.save();
-					context.translate(_cars[i].x, _cars[i].y);
-					context.rotate(_cars[i].rotate * _toRadians);
-					context.drawImage(_cars[i].skin, -(_cars[i].width/8), -(_cars[i].height/8), _cars[i].width/4, _cars[i].height/4);
-					context.restore();
-				}
+			while(i--) {	
+				context.save();
+				context.translate(_cars[i].x, _cars[i].y);
+				context.rotate(_cars[i].rotate * _toRadians);
+				context.drawImage(_cars[i].skin, -(_cars[i].width/8), -(_cars[i].height/8), _cars[i].width/4, _cars[i].height/4);
+				context.restore();
 			}
 		};
 
 		this.step = function(tileSize) {
 			var i = _cars.length;
 
-			while(i) {
-				if(_cars[--i].alive) {
-					_cars[i].drive(tileSize);
-				}
+			while(i--) {
+				_cars[i].drive(tileSize);	
 			}
 		};
 	}
